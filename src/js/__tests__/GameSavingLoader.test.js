05d14d144daeec20d47f3a8ede7330808bed4cf2
Astario <mokatev@yandex.ru>
2020-06-24 16:21:32 +0300

    test corrected

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,17 +1,19 @@
 import { GameSavingLoader } from '../GameSavingLoader';
 
 test('should work correct', (done) => {
-  let result = GameSavingLoader.load().then((saving) => saving);
-  result = JSON.parse(result);
-  expect(result).toEqual({
-    id: 9,
-    created: 1546300800,
-    userInfo: {
-      id: 1,
-      name: 'Hitman',
-      level: 10,
-      points: 2000,
-    },
+  const result = GameSavingLoader.load().then((saving) => saving);
+  // eslint-disable-next-line no-shadow
+  result.then((result) => {
+    expect(result).toEqual(JSON.stringify({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000,
+      },
+    }));
+    done();
   });
-  done();
 });
